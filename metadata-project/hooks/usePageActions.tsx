2b768fca4e6d2aaf7704b60939f322f8be8f3faf
feed-mina <myelin24@naver.com>
2026-01-25 19:22:35 +0900

    feat:일기저장 버튼 누르면 백앤드 /addDiaryList 컨트롤러 ìí사용

--- metadata-project/hooks/usePageActions.tsx
+++ metadata-project/hooks/usePageActions.tsx
@@ -35,14 +35,9 @@ export const usePageActions = (metadata: any[]) =>{
 
         // 3. 데이터 전송 로직
         if(actionType === "SUBMIT"){
-            // 1. 현재 데이터 로그 확인 (디버깅용)
+            //  현재 데이터 로그 확인 (디버깅용)
             console.log("보낼 데이터:", formData);
             // 필수 값 체크 및 데이터 가공
-            const emailId = formData["user_email"];
-            const password = formData["user_pw"];
-            const emailDomain = formData["user_email_domain"];
-            const fullEmail = (emailId && emailDomain) ? `${emailId}@${emailDomain}` : "";
-
             const requiredFields = metadata.filter(meta => meta.isRequired);
 
             for (const field of requiredFields as any[]){
@@ -53,20 +48,40 @@ export const usePageActions = (metadata: any[]) =>{
                     return;
                 }
             }
-// 3. 실제 전송할 URL 결정
+            // 실제 전송할 URL 결정
             const finalUrl = actionUrl || `/api/execute/${item.dataSqlKey}`;
-            const submitData = { ...formData, user_email: fullEmail };
-            if(!fullEmail){
-                return;
+            let submitData = { ...formData};
+            // 로그인 페이지인 경우 이메일 조합 로직 수행 _formData에 user_email_domain이 있을때만
+
+            const emailId = formData["user_email"];
+            const password = formData["user_pw"];
+            const emailDomain = formData["user_email_domain"];
+            if(emailId && emailDomain ) {
+                const fullEmail = `${emailId}@${emailDomain}`;
+                // submitData["email"] = fullEmail;
+                submitData = {...submitData, user_email: fullEmail};
             }
+
+            console.log("서버로 보낼 최종 데이터:",submitData);
 
             try{
                 const response = await axios.post(actionUrl, submitData);
+                // 성공 후 처리
+                if(response.status === 200|| response.status === 201){
+                    console.log('성공: ', submitData);
+                    alert('성공');
+                }
                 if(response.data.accessToken){
                     document.cookie = `accessToken=${response.data.accessToken}; path=/; max-age=3600`;
                     router.push("/view/MAIN_PAGE");
                 }
-                router.push("/view/MAIN_PAGE");
+                else {
+                    if(actionUrl.includes("addDiaryList")){
+                        router.push("/view/DIARY_LIST");
+                    } else{
+                        router.push("/view/MAIN_PAGE");
+                    }
+                }
             } catch (error) {
                 // const errorMsg = error.response?.data?.message || "로그인 정보를 확인해주세요";
                 console.log("error: ", error);
