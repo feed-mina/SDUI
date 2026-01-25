a3829cd95c30290424c4d7671d36c61c95d42bf0
feed-mina <myelin24@naver.com>
2026-01-25 21:12:59 +0900

    fix : RefreshToken @Id ì의존성 dependency error 수정

--- Ai-Diary-server/src/main/java/com/domain/demo_backend/token/domain/RefreshToken.java
+++ Ai-Diary-server/src/main/java/com/domain/demo_backend/token/domain/RefreshToken.java
@@ -2,16 +2,11 @@ package com.domain.demo_backend.token.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
-
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED) // 보안을 위해서...
 // 20260125 Redis 로직 추가
