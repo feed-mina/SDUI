f53cab9b18a097cb9ce64d15b49f38c745639662
feed-mina <myelin24@naver.com>
2026-02-04 21:09:25 +0900

    css 재배치

--- metadata-project/app/layout.tsx
+++ metadata-project/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";// layout.tsx 수정
-import "./globals.css";
-import "@/styles/common.css";
-import "@/styles/components.css";
-import "@/styles/pages.css";
+import "./globals.css";      // 1. 가장 기본 (Tailwind 등)
+import "./styles/common.css";   // 2. 공통 레이아웃
+import "./styles/components.css"; // 3. 공통 부품
+import "./styles/pages.css";    // 4. 페이지별 특수 스타일 (가장 우선순위 높음)
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider"; // 방금 만든 방 가져오기
 import RecordTimeComponent from "@/components/fields/RecordTimeComponent"
 
