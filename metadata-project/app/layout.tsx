b47895060e8cbfb301fad9dba3de57a56988d2c4
feed-mina <myelin24@naver.com>
2026-02-04 16:48:05 +0900

    fix:css 전역 중복 수정 및 배치

--- metadata-project/app/layout.tsx
+++ metadata-project/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";// layout.tsx 수정
 import "./globals.css";
-import './styles/main.css';
+import "@/styles/common.css";
+import "@/styles/components.css";
+import "@/styles/pages.css";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider"; // 방금 만든 방 가져오기
 import RecordTimeComponent from "@/components/fields/RecordTimeComponent"
-// (폰트나 메타데이터 설정은 그대로 두세요)
 
 export default function RootLayout({children,
                                    }: Readonly<{
