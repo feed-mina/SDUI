96dfe9d0a25821674addb7573253b98e165afb16
feed-mina <myelin24@naver.com>
2026-02-11 11:18:57 +0900

    feat:url 매핑맵

---
+++ metadata-project/components/constants/screenMap.ts
@@ -0,0 +1,10 @@
+export const SCREEN_MAP: Record<string, string> = {
+    "/": "MAIN_PAGE",
+    "/MAIN_PAGE": "MAIN_PAGE",
+    "/LOGIN_PAGE": "LOGIN_PAGE",
+    "/MY_PAGE": "MY_PAGE",
+    "/DASHBOARD_PAGE": "DASHBOARD_PAGE",
+    "/DIARY_LIST": "DIARY_LIST",
+};
+
+export const DEFAULT_SCREEN_ID = "COMMON_LAYOUT";
