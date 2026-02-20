54b8d77ea8ded874fe7d76765e795611caf6f7d5
feed-mina <myelin24@naver.com>
2026-02-20 20:44:02 +0900

    feat:필드별 렌더링 로그

--- metadata-project/components/utils/withRenderTrack.tsx
+++ metadata-project/components/utils/withRenderTrack.tsx
@@ -1,17 +1,20 @@
 import { useRenderCount } from "@/hooks/useRenderCount";
 import React from 'react';
+import {useMetadata} from "@/components/MetadataProvider";
 
 export function withRenderTrack<P extends object>(
     Component: React.ComponentType<P>,
     componentName: string
 ) {
-    // 1. 익명 함수 대신 이름을 가진 함수를 정의해
+    // 1. 익명 함수 대신 이름을 가진 함수로 정의하여 디버깅 효율을 높입니다.
     const WrappedComponent = (props: P) => {
-        useRenderCount(componentName);
+
+        // 1. 컨텍스트에서 현재 화면 ID를 가져온다
+        const { screenId } = useMetadata();
+        useRenderCount(`Field: ${componentName} (Screen: ${screenId})`);
         return <Component {...props} />;
     };
-
-    // 2. @@@@ 핵심: DevTools에 표시될 이름을 설정해 (에러 해결 지점) [cite: 2025-12-28]
+// 2. 핵심: 리액트 개발자 도구(DevTools)에서 컴포넌트를 식별할 수 있게 이름을 부여합니다.
     WrappedComponent.displayName = `WithRenderTrack(${componentName})`;
     return WrappedComponent;
 }
