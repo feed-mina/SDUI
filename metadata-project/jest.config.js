ee0c8fee02095fd9597f1a7a1c35feb964dc4deb
feed-mina <myelin24@naver.com>
2026-02-20 13:57:50 +0900

    test:테스트환경설정

--- metadata-project/jest.config.js
+++ metadata-project/jest.config.js
@@ -1,19 +1,41 @@
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({
-    dir: './',
-})
-
-const customJestConfig = {
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
-    transformIgnorePatterns: [
-        '/node_modules/(?!(swiper|ssr-window|dom7)/)',
-    ],
-    moduleNameMapper: {
-        // CSS 파일 로드 에러 방지
-        '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
-    },
-};
 
-module.exports = createJestConfig(customJestConfig);
+    transformIgnorePatterns: [
+        "/node_modules/(?!(until-async|msw|@mswjs|@bundled-es-modules|swiper|ssr-window|dom7)/)"
+    ],
+
+    transform: {
+        '^.+\\.(t|j)sx?|mjs$': ['@swc/jest'],
+    },
+
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^swiper/css$': 'identity-obj-proxy',
+        '^swiper/css/(.*)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/$1',
+        '^msw$': '<rootDir>/node_modules/msw',
+    },
+
+    testEnvironmentOptions: {
+        customExportConditions: ['node', 'node-addons'],
+    },
+    // @@@@ 테스트 파일 경로 규칙 추가
+    testMatch: [
+        "<rootDir>/tests/**/*.(test|spec).(ts|tsx)",
+        "<rootDir>/src/**/__tests__/**/*.(ts|tsx)"
+    ],
+    reporters: [
+        "default",  // 터미널 출력을 위해 기본 리포터 유지
+        ["jest-html-reporter", {
+            "pageTitle": "메타데이터 테스트 리포트",
+            // @@@@ 로그 폴더 지정: tests/logs 폴더 안에 생성되도록 설정
+            "outputPath": "./tests/logs/frontend-report.html",
+            "includeFailureMsg": true,
+            "dateFormat": "yyyy-mm-dd HH:MM:ss"
+        }],
+        "<rootDir>/tests/CustomReporter.js" // 커스텀 리포터 추가
+    ]
+};
--- metadata-project/jest.setup.js
+++ metadata-project/jest.setup.js
@@ -1,2 +1,29 @@
-// 가상 DOM 환경에서 사용할 매처들을 확장함
-import '@testing-library/jest-dom';
+// jest.setup.js
+
+// 1. Web Streams API 폴리필 추가 (WritableStream 등)
+import { WritableStream, ReadableStream, TransformStream } from 'node:stream/web';
+
+Object.defineProperties(global, {
+    WritableStream: { value: WritableStream },
+    ReadableStream: { value: ReadableStream },
+    TransformStream: { value: TransformStream },
+});
+
+// 2. 이전에 추가했던 BroadcastChannel 폴리필
+if (typeof global.BroadcastChannel === 'undefined') {
+    global.BroadcastChannel = class {
+        constructor(name) { this.name = name; }
+        postMessage(message) {}
+        onmessage = null;
+        close() {}
+        addEventListener() {}
+        removeEventListener() {}
+    };
+}
+
+// 3. fetch 및 TextEncoder 폴리필 (중요)
+import 'whatwg-fetch';
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
