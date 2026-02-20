cc3b3e12e4f339ae80f71687308fd159d9c824ef
feed-mina <myelin24@naver.com>
2026-02-20 13:53:37 +0900

    feat:JOIN FETCH 적용

--- Ai-Diary-server/src/main/java/com/domain/demo_backend/domain/diary/domain/DiaryRepository.java
+++ Ai-Diary-server/src/main/java/com/domain/demo_backend/domain/diary/domain/DiaryRepository.java
@@ -3,6 +3,7 @@ package com.domain.demo_backend.domain.diary.domain;
 import com.domain.demo_backend.domain.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,7 +18,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     int countByUserAndDelYn(User user, String delYn);
 
     // 특정 사용자의 일기 목록을 페이징하여 가져오기 (Spring Data JPA 명명 규칙 활용)
-    @Query("SELECT d FROM Diary d WHERE d.user.userSqno = :userSqno AND d.delYn = :delYn ORDER BY d.regDt DESC")
+    @Query("SELECT d FROM Diary d JOIN FETCH d.user WHERE d.user.userSqno = :userSqno AND d.delYn = :delYn ORDER BY d.regDt DESC")
     List<Diary> findMemberDiaryList(@Param("userSqno") Long userSqno, @Param("delYn") String delYn, Pageable pageable);
     // 2. 특정 사용자의 일기 목록을 페이징하여 가져오기
 
@@ -35,11 +36,14 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     // 3. 개수 세기
     int countByUserId(String userId);
 
-    // 4. 삭제되지 않은 일기 목록을 최신순으로 페이징하여 가져오기
+
+    //  전체 목록 페이징 조회 시 User도 같이 가져오기
+    @EntityGraph(attributePaths = {"user"})
     Page<Diary> findByDelYnOrderByRegDtDesc(String delYn, Pageable pageable);
 
+    //  특정 유저의 일기 목록 조회 시 User도 같이 가져오기
+    @EntityGraph(attributePaths = {"user"})
     Page<Diary> findByUserAndDelYnOrderByRegDtDesc(User user, String delYn, Pageable pageable);
-
 
     // User 객체 내부의 userSqno(또는 id)를 참조하도록 메서드명을 변경합니다.
 // User 엔티티 내부의 PK 필드명이 id라면 findByUserId, userSqno라면 findByUserUserSqno가 됩니다.
@@ -50,5 +54,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 //    List<Diary> findByUser_UserSqnoOrderByRegDtDesc(Long userSqno);
 
     // 엔티티의 private String userId 필드와 타입을 맞춥니다.
+//  필드명 기준 조회 시에도 적용 가능
+    @EntityGraph(attributePaths = {"user"})
     List<Diary> findByUserIdOrderByRegDtDesc(String userId);
 }
