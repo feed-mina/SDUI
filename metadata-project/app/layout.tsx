cc6425cc43fb511b1f885965639780de9ccf79f2
feed-mina <myelin24@naver.com>
2026-02-23 13:08:47 +0900

    feat:카카오주소api스크립트추가

--- metadata-project/app/layout.tsx
+++ metadata-project/app/layout.tsx
@@ -1,13 +1,10 @@
-import "./globals.css"; // 1. 가장 기본 (Tailwind 등)
-import "./styles/common.css"; // 2. 공통 레이아웃
-import "./styles/components.css"; // 3. 공통 부품
-import "./styles/pages.css"; // 4. 페이지별 특수 스타일 (가장 우선순위 높음)
-import "./styles/swap_inlinestyle.css" // 5. @@@@ 2026-02-07
+import "./styles/index.css";
+
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider"; // 방금 만든 방 가져오기
 import {MetadataProvider} from "@/components/MetadataProvider";
 import { AuthProvider } from '@/context/AuthContext';
-import Header from "@/components/Header";
 import AppShell from "@/components/AppShell";
+import Script from 'next/script';
 //  @@@@ 2026-02-08 수정 MetadataProvider 적용
 // layout.tsx 에 있는 컴포넌트들이 undefined 에러 없이 데이터를 안정적으로 받아오게 하는 API 흐름 설계
 // @@@@ layout 역할 :  프론트앤드 전체 레이아웃 구조
@@ -17,6 +14,13 @@ export default function RootLayout({children}: Readonly<{
     return (
         <html lang="ko">
         <body className="antialiased">
+        {/*   카카오 주소 API 스크립트 추가 */}
+        <Script
+            src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
+            strategy="afterInteractive" // 인터랙티브해진 직후 로드 (성능 최적화)
+        />
+
+
         {/* 가장 바깥에서 QueryClient를 공급한다 (식탁 차리기) */}
         <ReactQueryProvider>
             {/* screenId는 일단 전달하되, 나중에 URL 파라미터나 경로 기반으로 동적 처리할 것 */}
