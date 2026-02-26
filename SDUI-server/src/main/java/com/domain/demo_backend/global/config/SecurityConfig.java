f0334b1539678fa205ad07b8a1f431b6280111da
feed-mina <myelin24@naver.com>
2026-02-26 09:59:30 +0900

    fix: 탄력적 IP와 Vercel주소 추가

--- SDUI-server/src/main/java/com/domain/demo_backend/global/config/SecurityConfig.java
+++ SDUI-server/src/main/java/com/domain/demo_backend/global/config/SecurityConfig.java
@@ -91,9 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080",
-                "http://web-2025-version1.s3-website.ap-northeast-2.amazonaws.com",
-                "https://justsaying.co.kr", "http://justsaying.co.kr"));
+        configuration.setAllowedOrigins(List.of("https://sdui-delta.vercel.app", "http://localhost:3000", "http://localhost:8080","http://43.201.237.68","https://justsaying.co.kr", "http://justsaying.co.kr"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true); // 쿠키 허용
