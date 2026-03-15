12dae3a0880555176b46771e1b31c2a42186b3c4
feed-mina <myelin24@naver.com>
2026-03-15 14:21:54 +0900

    feat: setter추가

--- SDUI-server/src/main/java/com/domain/demo_backend/domain/ai/dto/ChatMessage.java
+++ SDUI-server/src/main/java/com/domain/demo_backend/domain/ai/dto/ChatMessage.java
@@ -3,8 +3,10 @@ package com.domain.demo_backend.domain.ai.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessage {
