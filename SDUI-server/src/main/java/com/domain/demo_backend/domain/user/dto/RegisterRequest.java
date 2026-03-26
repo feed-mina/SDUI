18c0b75b842f83be4ac13c303d09d5130c6e3ca0
feed-mina <myelin24@naver.com>
2026-03-26 11:42:20 +0900

    fix : 회원가입에서 500 에러 수정

--- SDUI-server/src/main/java/com/domain/demo_backend/domain/user/dto/RegisterRequest.java
+++ SDUI-server/src/main/java/com/domain/demo_backend/domain/user/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.domain.demo_backend.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,11 +14,12 @@ import java.time.LocalDate;
 public class RegisterRequest {
     private String userId;
     @NotBlank(message = "비밀번호는 필수입니다.")
+    @JsonAlias("pw")
     private String password;
     private String role;
 
     @NotBlank(message = "핸드폰 번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "올바른 핸드폰 번호 형식이 아닙니다.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])-?(?:\\d{3}|\\d{4})-?\\d{4}$", message = "올바른 핸드폰 번호 형식이 아닙니다.")
     private String phone;
 
     @Email(message = "유효한 이메일을 입력하세요.")
